9b9cb35ec543c48566cc270aa89dbc19ac64c8c9
maycon-douglas2005 <mayconchaves2005@gmail.com>
2024-12-09 19:27:02 -0300

    Adicionado a primeira fase (apenas a imagem de fundo)

--- The-Shooter-Game/src/Views/Container.java
+++ The-Shooter-Game/src/Views/Container.java
@@ -5,9 +5,17 @@ import javax.swing.JFrame;
 public class Container extends JFrame{
 	
 	public Container() {
+		add(new Fase_1());
+		
+		setVisible(true);
 		setTitle("The Shooter");
-		setSize(1024,728);
+		setSize(988, 700);
+		setLocationRelativeTo(null);
+		this.setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		
 	}
 	
 	
